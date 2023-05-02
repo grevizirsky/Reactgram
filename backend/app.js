15a0ea3dc44b5ffc98e118c7a871b906d66d70e5
Adriano Grevizirsky <grevizirsky@gmail.com>
2023-05-02 18:24:26 -0300

    test route

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,11 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+//routes
+const router = require("./routes/router.js")
+
+app.use(router)
+
 app.listen(port, () => {
     console.log(`App rodando na porta ${port}`)
 })
