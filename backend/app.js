6cb2910dba7752972e85ee1971e1b838ece6992e
Adriano Grevizirsky <grevizirsky@gmail.com>
2023-05-02 18:36:23 -0300

    middlewares

--- backend/app.js
+++ backend/app.js
@@ -13,8 +13,18 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+//solve CORS
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
+
+//upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))//static = pasta contem arquivos estaticos
+
+//DB connection
+require("./config/db.js")
+
 //routes
 const router = require("./routes/router.js")
+const { connection } = require("mongoose")
 
 app.use(router)
 
