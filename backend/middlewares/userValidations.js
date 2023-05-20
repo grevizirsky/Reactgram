d4a536d6583f0fd6223b46939d5d5cb4e5b94085
Adriano Grevizirsky <grevizirsky@gmail.com>
2023-05-20 16:52:52 -0300

    user update middleware

--- backend/middlewares/userValidations.js
+++ backend/middlewares/userValidations.js
@@ -41,8 +41,21 @@ const loginValidation = () => {
             .withMessage("A senha é obrigatória.")
     ]
 }
-    
+
+const userUpdateValidation = () => {
+    return[
+        body("name")
+            .optional()
+            .isLength({min:3})
+            .withMessage("O nome precisa de pelo menos 3 caracteres."),
+        body("password")
+            .optional()
+            .isLength({min:5})
+            .withMessage("A senha precisa ter no minimo 5 caracteres.")
+    ]
+}
     module.exports = {
     userCreateValidation,
     loginValidation,
+    userUpdateValidation,
 }
