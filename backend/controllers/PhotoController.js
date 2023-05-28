8f51708efd2fe5b4fe59ca46752353245a25d58d
Adriano Grevizirsky <grevizirsky@gmail.com>
2023-05-27 23:05:37 -0300

    delete photos

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -25,11 +25,41 @@ const insertPhoto = async(req, res) => {
         res.status(422).json({
             errors: ["Houve um problema, por favor tente novamente mais tarde."]
         })
+        return
     }
 
     res.status(201).json(newPhoto)
 }
 
+//remove a photo from DB
+const deletePhoto = async(req, res) => {
+    const {id} = req.params
+    const reqUser = req.user
+    try {
+        const photo = await Photo.findById(mongoose.Types.ObjectId(id))
+
+        //check if photo exists
+        if(!photo){
+            res.status(404).json({errors: ["Foto nao encontrada"]})
+            return
+        }
+
+        //check if photo belongs to user
+        if(!photo.userId.equals(reqUser._id)){
+            res.status(422).json({errors: ["Ocorreu um erro, por favor tente novamente mais tarde"]})
+        }
+
+        await Photo.findByIdAndDelete(photo._id)
+
+        res.status(200).json({id: photo._id, message: "Foto excluida com sucesso"})
+    } catch (error) {
+        res.status(404).json({errors: ["Foto nao encontrada"]})
+        return
+    }
+}
+
+
 module.exports = {
     insertPhoto,
+    deletePhoto
 }
