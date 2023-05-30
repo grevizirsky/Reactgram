f6d83b54d34a58269e9a62ee6027eb4818667a5a
Adriano Grevizirsky <grevizirsky@gmail.com>
2023-05-30 00:05:27 -0300

    like photo

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -119,6 +119,33 @@ const updatePhoto = async(req, res) => {
     res.status(200).json({photo, message: "Foto atualizada com sucesso"})
 }
 
+//like functionality
+const likePhoto = async(req, res) => {
+    const {id} = req.params
+    const reqUser = req.user
+
+    const photo = await Photo.findById(id)
+
+    //check if photo exist
+    if(!photo){
+        res.status(404).json({errors: ["Foto nao encontrada"]})
+        return
+    }
+
+    //check if user already liked the photo
+    if(photo.likes.includes(reqUser._id)){
+        req.status(422).json({errors: ["Voce ja curtiu a foto"]})
+        return
+    }
+
+    //put user id in likes array
+    photo.likes.push(reqUser._id)
+
+    photo.save()
+
+    res.status(200).json({photoId: id, userId: reqUser._id, message: "A foto foi curtida"})
+}
+
 
 module.exports = {
     insertPhoto,
@@ -126,5 +153,6 @@ module.exports = {
     getAllPhotos,
     getUserPhotos,
     getPhotoById,
-    updatePhoto
+    updatePhoto,
+    likePhoto
 }
--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 //Controllers
-const {insertPhoto, deletePhoto, getAllPhotos, getUserPhotos, getPhotoById, updatePhoto} = require("../controllers/PhotoController")
+const {insertPhoto, deletePhoto, getAllPhotos, getUserPhotos, getPhotoById, updatePhoto, likePhoto} = require("../controllers/PhotoController")
 
 //Middlewares
 const {photoInsertValidation, photoUpdateValidation} = require("../middlewares/photoValidation")
@@ -18,6 +18,7 @@ router.get("/", authGuard, getAllPhotos)
 router.get("/user/:id", authGuard, getUserPhotos)
 router.get("/:id", authGuard, getPhotoById)
 router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto)
+router.put("/like/:id", authGuard, likePhoto)
 
 
 module.exports = router
