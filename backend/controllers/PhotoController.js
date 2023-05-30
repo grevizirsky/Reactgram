1033fe1cd45bb02c1a2f5315347800478820a272
Adriano Grevizirsky <grevizirsky@gmail.com>
2023-05-29 23:46:10 -0300

    update photo

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -89,6 +89,36 @@ const getPhotoById = async(req, res) =>  {
     res.status(200).json(photo)
 }
 
+//update a photo
+const updatePhoto = async(req, res) => {
+    const {id} = req.params
+    const {title} = req.body
+
+    const reqUser = req.user
+
+    const photo = await Photo.findById(id)
+
+    //check if photo exist
+    if(!photo){
+        res.status(404).json({errors: ["Foto nao encontrada"]})
+        return
+    }
+
+    //check if photo belongs to user
+    if(!photo.userId.equals(reqUser._id)){
+        res.status(422).json({errors: ["Ocorreu um erro, por favor tente novamente mais tarde"]})
+        return
+    }
+
+    if(title){
+        photo.title = title
+    }
+
+    await photo.save()
+
+    res.status(200).json({photo, message: "Foto atualizada com sucesso"})
+}
+
 
 module.exports = {
     insertPhoto,
@@ -96,4 +126,5 @@ module.exports = {
     getAllPhotos,
     getUserPhotos,
     getPhotoById,
+    updatePhoto
 }
